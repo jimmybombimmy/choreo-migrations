465542d914a6e42a7e43941765433634bde6dc26
James Peters <jamespeters92@gmail.com>
2026-07-02 19:50:10 +0100

    feat: CHOR-7: add user_ids link to tast_lists and vice versa

--- alembic/versions/2026_06_30_2014-b3db3cf06450_add_users_table.py
+++ alembic/versions/2026_06_30_2014-b3db3cf06450_add_users_table.py
@@ -20,7 +20,7 @@ depends_on: Union[str, Sequence[str], None] = None
 
 # This will definitely need better authentication in the future
 def upgrade() -> None:
-    """Add users table."""
+    """Add users table and add users column to task_lists."""
     op.create_table(
         "users",
         Column("id", UUID, primary_key=True),
@@ -29,12 +29,15 @@ def upgrade() -> None:
         Column("password", VARCHAR(30), nullable=False),
         Column("created_at", TIMESTAMP, server_default=func.now(), nullable=False),
         Column("updated_at", TIMESTAMP),
-        Column("collection_ids", ARRAY(UUID)),  # UUIDs of "collections.id"
+        Column("task_list_ids", ARRAY(UUID)),  # UUIDs of "collections.id"
     )
     pass
+
+    op.add_column("task_lists", Column("user_ids", ARRAY(UUID)))  # UUIDs of "users.id"
 
 
 def downgrade() -> None:
     """Remove users table."""
     op.drop_table("users")
+    op.drop_column("task_lists", "user_ids")
     pass
